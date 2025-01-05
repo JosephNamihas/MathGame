0f1a778026ad3956fe74018d315dc66b94bab45b
Joe Namihas <josephnamihas@yahoo.co.uk>
2025-01-05 20:18:13 +0000

    Pushed

--- obj/Debug/net8.0/MathGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/MathGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MathGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfaa5aef948421cd33fc650c3b4ed133ea8f26d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7273f9f8ef3b71c5ad308fa8b4c42f4a6cb0ac4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MathGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MathGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
